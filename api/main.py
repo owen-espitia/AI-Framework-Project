6220872aaedf564f317ecc1739379c76670c4261
Owen Espitia <oespitia@student.neumont.edu>
2026-04-19 21:29:11 -0600

    pushing dockerfile changes to main

---
+++ api/main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Your Pydantic model(s) here
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str
+
+
+
+# In-memory storage
+items_db: dict[int, dict] = {}
+next_id: int = 1
+
+# Your endpoints here
+# @app.get("/", status_code=200)
+# def landing_page():
+#     return "Welcome to my first python API!"
+
+@app.get("/items", status_code=200)
+def read_items():
+    return items_db
+
+@app.get("/items/{item_id}", status_code=200)
+def get_item(item_id: str):
+    item = items_db.get(int(item_id))
+    if item is None:
+        raise HTTPException(status_code=404, detail="No items with that ID")
+    return item
+
+@app.post("/items", status_code=201)
+def create_item(item: Item):
+    global next_id
+    item_id = next_id
+    items_db[item_id] = item.model_dump()
+    next_id += 1
+    return {"id": item_id, "name": item.name}
+
+@app.put("/items/{item_id}", status_code=200)
+def update_item(new_item: Item, item_id: str):
+    try:
+        items_db[int(item_id)] = new_item
+        return {"id": item_id, "updated_item": new_item}
+    except:
+        raise HTTPException(status_code=404, detail="No item with specified ID")
+
+@app.delete("/items/{item_id}", status_code=200)
+def delete_item(item_id: str):
+    try:
+        del items_db[int(item_id)]
+        return {"message": f"Item with ID {item_id} deleted successfully."}
+    except:
+        raise HTTPException(status_code=404, detail="No item with specified ID")
+
+app.mount("/", StaticFiles(directory="front-end/static", html=True), name="static")
